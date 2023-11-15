42a160e6fa33f01f76a79f05d7cad229bdacf1b9
Marcos <marcossosa21415@gmail.com>
2023-11-15 19:38:34 -0300

    Token guardado en base de datos

--- src/routes/token.js
+++ src/routes/token.js
@@ -1,19 +1,26 @@
-const express = require('express')
-const router = express.Router()
-const jwt = require('jsonwebtoken')
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const db = require('../db')
 const { serialize } = require('cookie');
 
 const secretKey = '48253755061145888295'
 
 router.get('/', (req, res) =>{
-    res.send("Que onda perro");
+    res.send("Que onda ");
 })
 
 router.post("/create", (req, res)=>{
     try{
-        const token = jwt.sign({ username: req.body.username }, secretKey, { expiresIn: '1h' });
-        res.json({mensaje: "Token generado correctamente", token});
-        console.log("El token se creo  todo piola");
+        const {username, password} = req.body
+        const token = jwt.sign({ username: username, password: password }, secretKey, { expiresIn: '1h' });
+        if(token){
+            const query = 'UPDATE usuarios SET token = ? WHERE username = ?'
+            db.query(query, [token, username], (err, result)=>{
+                if(err) res.json({msg: "Hubo un error al actualizar el token", err})
+                else res.json({msg: "El token se actualizo bien", result, token})
+            })
+        }
     }
     catch(err){
         return res.json({mensaje: "Hubo un error al crear el token", err})
@@ -21,11 +28,22 @@ router.post("/create", (req, res)=>{
 })
 
 router.post("/verify", (req, res)=>{
+    const {token} = req.body
     try{
-        jwt.verify(req.body.token, secretKey, (err, decoded)=>{
-            if(err) return res.json({msg: "El token no es valido", err})
-            else res.json({msg: "valido", info: decoded})
+        const query = 'SELECT token FROM usuarios WHERE token = ?'
+        db.query(query, [token], (err, result)=>{
+            if(err) res.json({msg: "El token no se encontro en la base de datos", err})
+            else{
+                jwt.verify(token, secretKey, (error, decoded)=>{
+                    if(error) res.json({valido: false, err})
+                    else res.json({valido: true, info: decoded})
+                })
+            }
         })
+        /* jwt.verify(token, secretKey, (err, decoded)=>{
+            if(err) res.json({valido: false, err})
+            else res.json({valido: true, info: decoded})
+        }) */
     }
     catch(err){
         return res.json({mensaje: "Hubo un error al verificar el token", err})
