e016a89908d29e120f7f0d11266b693c73530d8f
Marcos <126882939+Markucho18@users.noreply.github.com>
2023-11-16 17:53:02 -0300

    obtenerUsuario con query
    El frontend puede obtener el id usuario mandando el username como query en el url. Me tira error al verificar token (token/verify)

--- src/routes/tareas.js
+++ src/routes/tareas.js
@@ -1,70 +1,58 @@
 //tareas.js
-const express = require('express');
-const db = require('../db');
+const express = require("express");
+const db = require("../db");
 const router = express.Router();
 
-router.get("/", (req, res) =>{
-    const query = 'SELECT * FROM tareas'
-    db.query(query, (err, result)=>{
-        if(err){
-            console.error('Error al ejecutar la consulta', err);
-            return
-        }
-        res.send(result)
-        console.log(result);
-    })
-})
+router.get("/", (req, res) => {
+  const query = "SELECT * FROM tareas";
+  db.query(query, (err, result) => {
+    if (err) {
+      console.error("Error al ejecutar la consulta", err);
+      return;
+    }
+    res.send(result);
+    console.log(result);
+  });
+});
 
-router.get("/inbox", (req, res)=>{
-    const query = "SELECT * FROM `tareas` WHERE `fecha` IS NULL"
-    db.query(query, (err, result)=>{
-        if(err){
-            console.error('Error al ejecutar la consulta', err);
-            return
-        }
-        res.send(result)
-        console.log(result);
-    })
-})
-
-router.get("/hoy", (req, res) =>{
-    const query = "SELECT * FROM `tareas` WHERE `fecha` = CURDATE()"
-    db.query(query, (err, result)=>{
-        if(err){
-            console.error('Error al ejecutar la consulta', err);
-            return
-        }
-        res.send(result)
-        console.log(result);
-    })
-})
-
-router.get("/proximo", (req, res) =>{
-    const query = "SELECT * FROM `tareas` WHERE `fecha` != CURDATE()"
-    db.query(query, (err, result)=>{
-        if(err){
-            console.error('Error al ejecutar la consulta', err);
-            return
-        }
-        res.send(result)
-        console.log(result);
-    })
-})
-
-router.post("/", (req, res) =>{
-    res.send("Hiciste una peticion POST en /tareas")
-})
-
-router.put("/", (req, res) =>{
-    res.send("Hiciste una peticion PUT en /tareas")
-})
-
-router.delete("/", (req, res) =>{
-    res.send("Hiciste una peticion DELETE en /tareas")
-})
-
-router.patch("/", (req, res) =>{
-    res.send("Hiciste una peticion PATCH en /tareas")
+router.post("/crear", (req, res)=>{
+    const query = "INSERT INTO tareas ()"
 })
 
 module.exports = router;
+
+/* router.get("/inbox", (req, res)=>{
+        const query = "SELECT * FROM `tareas` WHERE `fecha` IS NULL"
+        db.query(query, (err, result)=>{
+            if(err){
+                console.error('Error al ejecutar la consulta', err);
+                return
+            }
+            res.send(result)
+            console.log(result);
+        })
+    })
+    
+    router.get("/hoy", (req, res) =>{
+        const query = "SELECT * FROM `tareas` WHERE `fecha` = CURDATE()"
+        db.query(query, (err, result)=>{
+            if(err){
+                console.error('Error al ejecutar la consulta', err);
+                return
+            }
+            res.send(result)
+            console.log(result);
+        })
+    })
+    
+    router.get("/proximo", (req, res) =>{
+        const query = "SELECT * FROM `tareas` WHERE `fecha` != CURDATE()"
+        db.query(query, (err, result)=>{
+            if(err){
+                console.error('Error al ejecutar la consulta', err);
+                return
+            }
+            res.send(result)
+            console.log(result);
+        })
+    }) */
--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -15,4 +15,13 @@ router.get("/", (req, res)=>{
     })
 })
 
+router.get("/obtener", (req, res)=>{
+    const usuario = req.query.usuario;
+    const query = 'SELECT id_usuario FROM usuarios WHERE username = ?'
+    db.query(query, [usuario], (err, result)=>{
+        if(err) res.json({msg:"Hubo un error al consultar usuarios SQL: ", err})
+        else res.json({msg: "La consulta SQL a usuarios salio bien", result})
+    })
+})
+
 module.exports = router;
